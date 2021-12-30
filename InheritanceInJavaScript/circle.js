b9fd4e07336bbb020b5bca1d87e47d8a6db1a612
Amul Sharma <94516019+Amulsharma64@users.noreply.github.com>
2021-12-30 23:20:41 +0530

    go ahead

---
+++ InheritanceInJavaScript/circle.js
@@ -0,0 +1,16 @@
+var Shape = require('./shape.js');
+// declare class
+class Circle extends Shape {
+     constructor(color) {
+          super();
+     }
+     calculateArea() {
+
+     }
+}
+
+// export class using module.exports
+module.exports = Circle; 
+
+
+
---
+++ InheritanceInJavaScript/shape.js
@@ -0,0 +1,15 @@
+//shapes.js
+ // declare class
+class Shape {
+     constructor() {
+          this.color = "green";
+     }
+     drawShape() {
+
+     }
+     calculateArea() {
+
+     }
+}
+// export class using module.exports
+module.exports = Shape;
